918b3648f79b9ba73079d49f466422c0655ab12b
MHOPI98 <bdmhopi@gmail.com>
2024-03-20 00:47:17 +0600

    Math

---
+++ math.js
@@ -0,0 +1,9 @@
+ const min =Math.min(45,21,3,55,10);
+ console.log(min);
+ const max =Math.max(45,21,3,55,10);
+ console.log(max);
+ console.log(Math.PI);
+ console.log(Math.round(4.5));
+ console.log(Math.floor(4.5));
+ console.log(Math.ceil(4.5));
+ console.log(Math.random());
