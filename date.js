0180da4b9e1a64d02fd837189d0909be99521caf
MHOPI98 <bdmhopi@gmail.com>
2024-03-20 16:55:03 +0600

    math

--- date.js
+++ date.js
@@ -1,3 +1,9 @@
 const today= new Date();
 console.log(today.toLocaleString('en-GB'));
 
+for(var i=0; i<5; i++){
+    setTimeout(function()
+    {
+        console.log(i);
+    },100);
+}
--- math.js
+++ math.js
@@ -5,5 +5,9 @@
  console.log(Math.PI);
  console.log(Math.round(4.5));
  console.log(Math.floor(4.5));
- console.log(Math.ceil(4.5));
+ console.log(Math.ceil(7.004));
  console.log(Math.random());
+ console.log(Math.pow(2,3));
+ console.log(Math.abs(-8.258));
+ console.log(Math.sqrt(2));
+
